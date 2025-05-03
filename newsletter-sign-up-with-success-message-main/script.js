a05067ceb58a1a4cf69d2e34d50d7b49d652145f
Elai5 <yvettelaine007@gmail.com>
2025-05-03 07:06:19 +0300

    email-form works

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -7,10 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
   const message = document.getElementById("message");
   const dismissBtn = document.getElementById("dismiss");
 
-  button.addEventListener("click", () => {
-    card.classList.remove("hidden");
-    content.classList.add("hidden");
-    message.classList.remove("hidden");
+  const form = document.getElementById("email-form");
+  const emailInput = document.getElementById("email");
+  const messageError = document.getElementById("email-error");
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailValue = emailInput.value.trim();
+
+    if (!regex.test(emailValue)) {
+      messageError.style.display = "block";
+      emailInput.classList.add("border-red-400");
+      emailInput.classList.remove("border-gray-200");
+    } else {
+      messageError.style.display = "none";
+      emailInput.classList.remove("border-red-400");
+      emailInput.classList.add("border-gray-200");
+
+      emailInput.value = "";
+
+      card.classList.remove("hidden");
+      content.classList.add("hidden");
+      message.classList.remove("hidden");
+    }
   });
 
   dismissBtn.addEventListener("click", () => {
