7c5236354d080ee2ced8a4556e10dc3445272feb
Elai5 <yvettelaine007@gmail.com>
2025-05-12 11:47:49 +0300

    slider arrow remove, buttons enlarge and flex

--- space/src/assets/assets.js
+++ space/src/assets/assets.js
@@ -38,30 +38,27 @@ export const assets = {
 };
 export const crew = [
   {
-    name: "Douglas Hurley",
+    name: "DOUGLAS HURLEY",
     images: imageDouglas,
-    role: "Commander",
+    role: "COMMANDER",
     bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
   },
   {
     name: "Mark Shuttleworth",
     images: imageMark,
-    role: "Mission Specialist",
+    role: "MISSION SPECIALIST",
     bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
   },
   {
     name: "Victor Glover",
     images: imageVictor,
-    role: "Pilot",
+    role: "PILOT",
     bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
   },
   {
     name: "Anousheh Ansari",
-    images: {
-      png: "./assets/crew/image-anousheh-ansari.png",
-      webp: "./assets/crew/image-anousheh-ansari.webp",
-    },
-    role: "Flight Engineer",
+    images: imageAnousheh,
+    role: "FLIGHT ENGINEER",
     bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
   },
 ];
--- space/src/components/Crew.jsx
+++ space/src/components/Crew.jsx
@@ -10,6 +10,7 @@ import "slick-carousel/slick/slick-theme.css";
 const Crew = () => {
   const settings = {
     dots: true,
+    arrows: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -22,47 +23,36 @@ const Crew = () => {
         style={{ backgroundImage: `url(${assets.backgroundCrew})` }}
         id="crew"
       >
-        <div className="w-full flex flex-col px-20 py-5">
+        <div className="w-full flex flex-col gap-20 px-20 py-5">
           <div>
             <h2 className="text-white">
               <span className="text-gray-600 mr-5 ">01</span>MEET YOUR CREW
             </h2>
           </div>
-          <div className="w-full gap-3">
-            <Slider {...settings}>
+          <div className="w-full flex flex-col gap-5 px-10">
+            <Slider {...settings} className="">
               {crew.map((member, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col-2 items-center justify-between gap-10 p-6 text-white"
-                >
-                  <div>
-                    <h2 className="text-base">{member.role}</h2>
-                    <h1 className="text-xl">{member.name}</h1>
-                    <p className="text-sm">{member.bio}</p>
-                  </div>
-                  <div className="flex-1 flex justify-center">
-                    <img
-                      src={member.images}
-                      alt={member.name}
-                      className="w-64 h-auto object-contain"
-                    />
-                    {/* {member.image && (
+                <div key={index} className="w-full">
+                  <div className="flex flex-col md:flex-row items-center gap-1 w-full">
+                    <div className="flex flex-col gap-3 ">
+                      <h2 className="text-base text-gray-400">{member.role}</h2>
+                      <h1 className="text-5xl text-white uppercase">
+                        {member.name}
+                      </h1>
+                      <p className="text-sm text-gray-300 w-3/4 leading-6.5">
+                        {member.bio}
+                      </p>
+                    </div>
+                    <div className="w-full flex justify-center items-center ">
                       <img
-                        src={member.imageDouglas}
-                        alt=""
-                        srcset=""
+                        src={member.images}
+                        alt={member.name}
                         className="w-64 h-auto object-contain"
                       />
-                    )} */}
+                    </div>
                   </div>
-                  {/* 
-                  {member.images && <img src={member.imageDouglas} alt="" />} */}
                 </div>
               ))}
-
-              <div>
-                <h2>hi</h2>
-              </div>
             </Slider>
           </div>
         </div>
