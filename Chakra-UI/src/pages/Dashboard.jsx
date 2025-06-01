e3625ddd2a9540de2d046075d2a72e6dd4824fbe
Elai5 <yvettelaine007@gmail.com>
2025-06-01 20:00:53 +0300

    added chakra,changed component,teste heading,text and container

--- Chakra-UI/src/pages/Dashboard.jsx
+++ Chakra-UI/src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
+/** @format */
+import { Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Container>
+      <Heading my="30px" p="10px">
+        Chakra UI components
+      </Heading>
+      <Text>
+        Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aspernatur praesentium deleniti veniam?
+      </Text>
+    </Container>
+  );
 }
