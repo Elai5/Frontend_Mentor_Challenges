48969a9da94250184aa41e5115ea0fef8941cc60
Elai5 <yvettelaine007@gmail.com>
2025-06-01 20:57:37 +0300

    used alignite,s to center items on nav

--- Chakra-UI/src/components/Navbar.jsx
+++ Chakra-UI/src/components/Navbar.jsx
@@ -1,23 +1,32 @@
 /** @format */
 
 // import React from 'react'
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Heading, Text, Button, Spacer, HStack } from "@chakra-ui/react";
 
 export const Navbar = () => {
   return (
-    <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
-      <Box w="150px" h="50px" bg="red">
-        1
+    <Flex as="nav" p="10px" alignItems="center" gap="10px">
+      <Heading as="h1">Doja Tasks</Heading>
+      <Spacer></Spacer>
+      <Box bg="gray.200" p="10px">
+        M
       </Box>
-      <Box w="150px" h="50px" bg="blue">
-        2
-      </Box>
-      <Box w="150px" h="50px" bg="green" flexGrow="1">
-        3
-      </Box>
-      <Box w="150px" h="50px" bg="yellow" flexGrow="2">
-        4
-      </Box>
+      <Text>gray@netnija.dev</Text>
+      <Button>LogOut</Button>
     </Flex>
+    // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
+    //   <Box w="150px" h="50px" bg="red">
+    //     1
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="blue">
+    //     2
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="green" flexGrow="1">
+    //     3
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="yellow" flexGrow="2">
+    //     4
+    //   </Box>
+    // </Flex>
   );
 };
