da5ec4dff5a91c83c862d3707c0df14a005785b0
Elai5 <yvettelaine007@gmail.com>
2025-06-01 20:28:10 +0300

    use as to specify style props

--- Chakra-UI/src/pages/Dashboard.jsx
+++ Chakra-UI/src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 /** @format */
-import { Container, Heading, Text } from "@chakra-ui/react";
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
-    <Container>
+    <Container as="section" maxW="4xl">
       <Heading my="30px" p="10px">
         Chakra UI components
       </Heading>
@@ -11,6 +11,13 @@ export default function Dashboard() {
         Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Aspernatur praesentium deleniti veniam?
       </Text>
+      <Text ml="30px" color="blue.300" fontWeight="bold">
+        Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aspernatur praesentium deleniti veniam?
+      </Text>
+      <Box my="30px" p="20px" bg="gold">
+        <Text color="white">I have a box</Text>
+      </Box>
     </Container>
   );
 }
