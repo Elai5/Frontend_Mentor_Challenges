eed2f00c0e2f20c411ccaff97a53782aa85d4725
Elai5 <yvettelaine007@gmail.com>
2025-06-01 20:59:12 +0300

    use HSTack to center align items insted of align items

--- Chakra-UI/src/components/Navbar.jsx
+++ Chakra-UI/src/components/Navbar.jsx
@@ -1,18 +1,29 @@
 /** @format */
 
 // import React from 'react'
-import { Flex, Box, Heading, Text, Button, Spacer, HStack } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Spacer,
+  HStack,
+} from "@chakra-ui/react";
 
 export const Navbar = () => {
   return (
-    <Flex as="nav" p="10px" alignItems="center" gap="10px">
+    <Flex as="nav" p="10px">
       <Heading as="h1">Doja Tasks</Heading>
       <Spacer></Spacer>
-      <Box bg="gray.200" p="10px">
-        M
-      </Box>
-      <Text>gray@netnija.dev</Text>
-      <Button>LogOut</Button>
+
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">
+          M
+        </Box>
+        <Text>gray@netnija.dev</Text>
+        <Button>LogOut</Button>
+      </HStack>
     </Flex>
     // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
     //   <Box w="150px" h="50px" bg="red">
