92012ce8d3d5599089bfc6c853e5168f5d71488c
Elai5 <yvettelaine007@gmail.com>
2025-06-01 20:46:58 +0300

    added flex gro on diffrent components and flex wrap

--- Chakra-UI/src/components/Navbar.jsx
+++ Chakra-UI/src/components/Navbar.jsx
@@ -5,17 +5,17 @@ import { Flex, Box } from "@chakra-ui/react";
 
 export const Navbar = () => {
   return (
-    <Flex bg="gray.200">
+    <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
       <Box w="150px" h="50px" bg="red">
         1
       </Box>
       <Box w="150px" h="50px" bg="blue">
         2
       </Box>
-      <Box w="150px" h="50px" bg="green">
+      <Box w="150px" h="50px" bg="green" flexGrow="1">
         3
       </Box>
-      <Box w="150px" h="50px" bg="yellow">
+      <Box w="150px" h="50px" bg="yellow" flexGrow="2">
         4
       </Box>
     </Flex>
