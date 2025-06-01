3ab69281b4a1ce9a188679cca88b9f2516db2078
Elai5 <yvettelaine007@gmail.com>
2025-06-01 20:44:13 +0300

    added flex for boxes

---
+++ Chakra-UI/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+/** @format */
+
+// import React from 'react'
+import { Flex, Box } from "@chakra-ui/react";
+
+export const Navbar = () => {
+  return (
+    <Flex bg="gray.200">
+      <Box w="150px" h="50px" bg="red">
+        1
+      </Box>
+      <Box w="150px" h="50px" bg="blue">
+        2
+      </Box>
+      <Box w="150px" h="50px" bg="green">
+        3
+      </Box>
+      <Box w="150px" h="50px" bg="yellow">
+        4
+      </Box>
+    </Flex>
+  );
+};
