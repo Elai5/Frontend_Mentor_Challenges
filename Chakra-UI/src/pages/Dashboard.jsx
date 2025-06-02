da6096988eb2c0514c47878bf1008f509eaa1e6d
Elai5 <yvettelaine007@gmail.com>
2025-06-02 16:36:48 +0300

    added card component,card header,body and footer

--- Chakra-UI/src/pages/Dashboard.jsx
+++ Chakra-UI/src/pages/Dashboard.jsx
@@ -1,5 +1,19 @@
 /** @format */
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { EditIcon, ViewIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  SimpleGrid,
+  Text,
+  Flex,
+  Heading,
+  HStack,
+  Button,
+  Divider,
+} from "@chakra-ui/react";
 import { div } from "framer-motion/client";
 import { useLoaderData } from "react-router-dom";
 
@@ -8,7 +22,41 @@ export default function Dashboard() {
 
   return (
     <SimpleGrid spacing={10} minChildWidth="300px">
-      {tasks && tasks.map((task) => <div key={task.id}>{task.title}</div>)}
+      {tasks &&
+        tasks.map((task) => (
+          <Card key={task.id}>
+            <CardHeader>
+              <Flex>
+                <Box w="50px" h="50px">
+                  <Text>AV</Text>
+                </Box>
+                <Box>
+                  <Heading as={"h3"} size={"sm"}>
+                    {task.title}
+                  </Heading>
+                  <Text>by {task.author}</Text>
+                </Box>
+              </Flex>
+            </CardHeader>
+            <CardBody color={"gray.500"}>
+              {" "}
+              <Text>{task.description}</Text>
+            </CardBody>
+
+            <Divider borderColor={"gray.200"} />
+            <CardFooter>
+              {" "}
+              <HStack>
+                <Button variant="ghost" leftIcon={<ViewIcon />}>
+                  Watch
+                </Button>
+                <Button variant={"ghost"} leftIcon={<EditIcon />}>
+                  Comment
+                </Button>
+              </HStack>
+            </CardFooter>
+          </Card>
+        ))}
     </SimpleGrid>
   );
 }
