43f92ee24955c530c2e3e3208b22d0f8062777cb
Elai5 <yvettelaine007@gmail.com>
2025-06-14 00:56:01 +0300

    added sidebar with drawer

--- Shoe-Ecommerce/src/components/TopNavbar.jsx
+++ Shoe-Ecommerce/src/components/TopNavbar.jsx
@@ -1,5 +1,7 @@
 /** @format */
 
+import React from "react";
+
 import {
   Box,
   Flex,
@@ -10,22 +12,13 @@ import {
   Image,
   useDisclosure,
   IconButton,
-  // Collapse,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  DrawerBody,
-  VStack,
+  Collapse,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 // import { HambugerIcon, CloseIcon } from "@chakra-ui/icons";
 
-import { assets } from "../assets/asset";
-import { NavLink } from "react-router-dom";
-
-const Navbar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+const TopNavbar = () => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
     <Box borderBottom={"1px solid"} borderColor={"#b6bcc8"}>
       <Flex
@@ -47,10 +40,10 @@ const Navbar = () => {
           <Box display={"flex"} gap={1} alignItems={"center"}>
             <Box>
               <IconButton
-                icon={<HamburgerIcon />}
-                aria-label="Open Menu"
+                icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                aria-label="Toggle Menu"
                 display={{ base: "inline-flex", md: "none" }}
-                onClick={onOpen}
+                onClick={onToggle}
                 variant={"ghost"}
               />
             </Box>
@@ -86,34 +79,34 @@ const Navbar = () => {
         </HStack>
         {/* <Divider></Divider> */}
       </Flex>
-      <Drawer placement="left" onClose={onClose} isOpen={isOpen} size={"xs"}>
-        <DrawerOverlay />
-        <DrawerContent w="75vw" maxW={"75vw"} bg={"white"}>
-          <DrawerCloseButton
-            mt={3}
-            px={7}
-            position="right"
-            color={"#1d2025"}
-            fontWeight={"bolder"}
-          />
-          <DrawerBody mt={5}>
-            <VStack
-              align="start"
-              spacing={4}
-              fontWeight="bolder"
-              fontStyle={"Kumbh Sans"}
-            >
-              <Link href="#">Collections</Link>
-              <Link href="#">Men</Link>
-              <Link href="#">Women</Link>
-              <Link href="#">About</Link>
-              <Link href="#">Contact</Link>
-            </VStack>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
+      <Collapse in={isOpen} animateOpacity>
+        <Box
+          display={{ md: "none" }}
+          px={4}
+          pb={4}
+          color={"blackAlpha.900"}
+          fontWeight={"bold"}
+          width={"150px"}
+        >
+          <Link display="block" py={2} href="#">
+            Collections
+          </Link>
+          <Link display="block" py={2} href="#">
+            Men
+          </Link>
+          <Link display="block" py={2} href="#">
+            Women
+          </Link>
+          <Link display="block" py={2} href="#">
+            About
+          </Link>
+          <Link display="block" py={2} href="#">
+            Contact
+          </Link>
+        </Box>
+      </Collapse>
     </Box>
   );
 };
 
-export default Navbar;
+export default TopNavbar;
