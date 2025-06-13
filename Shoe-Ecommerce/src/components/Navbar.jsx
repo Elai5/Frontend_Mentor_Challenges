822319e27e0a44d155b7a21cd2f0436448b3e0d5
Elai5 <yvettelaine007@gmail.com>
2025-06-13 22:44:57 +0300

    added avator instead of image component for image header

--- Shoe-Ecommerce/src/components/Navbar.jsx
+++ Shoe-Ecommerce/src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import {
   Link,
   HStack,
   Divider,
+  Avatar,
 } from "@chakra-ui/react";
 import React from "react";
 import { assets } from "../assets/asset";
@@ -18,20 +19,25 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <Box>
-      <Flex bg={"red.400"}>
-        <Flex gap={5}>
-          <img src={assets.logo} alt="" />
-          <HStack gap={4}>
-            <Link href="">Collections</Link>
-            <Link href="">Collections</Link>
-            <Link href="">About</Link>
-            <Link href="">Contacts</Link>
-          </HStack>
-        </Flex>
-        <Flex gap={4}>
+      <Flex bg={"red.900"} justifyContent={"space-around"}>
+        {/* <Flex gap={5} justifyContent={"space-around"}> */}
+        <img src={assets.logo} alt="" />
+        <HStack gap={4}>
+          <Link href="">Collections</Link>
+          <Link href="">Men</Link>
+          <Link href="">Women</Link>
+          <Link href="">About</Link>
+          <Link href="">Contacts</Link>
+        </HStack>
+        <HStack>
           <img src={assets.cartIcon} alt="" />
-          <img src={assets.imageAvatar} alt="" />
-        </Flex>
+          <Avatar
+            src={assets.imageAvatar}
+            border={"1px"}
+            borderColor={"#ff7d1a"}
+          ></Avatar>
+        </HStack>
+        {/* </Flex> */}
       </Flex>
     </Box>
   );
